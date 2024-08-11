59a09b9dfb3d93f0a51c3dbe1504f65dd381d05f
Maximiliano Landriel <maxiilandriel12@gmail.com>
2024-08-10 21:28:16 -0300

    Update inicio.dart

--- lib/widgets/inicio/inicio.dart
+++ lib/widgets/inicio/inicio.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:infinite_carousel/infinite_carousel.dart';
-import '../modal_navbar.dart';
 import '../navbar.dart';
-import '../inicio/testimonios.dart';
+
 
 class Inicio extends StatefulWidget {
   @override
@@ -46,224 +45,149 @@ class _InicioState extends State<Inicio> {
         preferredSize: const Size.fromHeight(80), // Altura del NavBar
         child: NavBar(), // Usa el NavBar como appBar
       ),
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/background.jpg'), // Imagen de fondo
-            fit: BoxFit.cover,
-          ),
-        ),
+            body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            // Banner en la parte superior
-            Container(
-              height: 150, // Ajusta la altura del banner según sea necesario
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/images/banner.jpeg'), // Cambia la ruta de la imagen según corresponda
-                  fit: BoxFit.cover,
-                ),
-              ),
-              child: Row(
-                children: [
-                  // Logo a la izquierda
-                  Container(
-                    margin: const EdgeInsets.all(16),
-                    child: Image.asset(
-                      'assets/images/logo_spa.png', // Cambia la ruta del logo según corresponda
-                      width: 150, // Ajusta el ancho del logo según sea necesario
-                      height: 150, // Ajusta la altura del logo según sea necesario
+            Row(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black),
+                    ),
+                    height: 400,
+                    child: InfiniteCarousel.builder(
+                      controller: _carouselController,
+                      itemCount: images.length,
+                      itemExtent: 400,
+                      center: true,
+                      anchor: 0.0,
+                      velocityFactor: 0.5,
+                      onIndexChanged: (index) {
+                        print('index: $index');
+                      },
+                      itemBuilder: (context, itemIndex, realIndex) {
+                        return Container(
+                          margin: const EdgeInsets.all(10),
+                          child: Image.asset(
+                            images[itemIndex],
+                            fit: BoxFit.cover,
+                          ),
+                        );
+                      },
                     ),
                   ),
-                  // Texto del banner a la derecha del logo
-                  Expanded(
-                    child: Center(
+                ),
+                const SizedBox(width: 16),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black),
+                    ),
+                    height: 400,
+                    child: const Center(
                       child: Text(
-                        'Bienvenido a Sentirse Bien', // Texto del banner
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 32,
-                          fontWeight: FontWeight.bold,
-                          shadows: [
-                            Shadow(
-                              blurRadius: 10.0,
-                              color: Colors.black.withOpacity(0.6),
-                              offset: const Offset(0, 2),
-                            ),
-                          ],
+                        'Sumérgete en el bienestar',
+                        style: TextStyle(fontSize: 24),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black),
+                    ),
+                    height: 150,
+                    child: const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        'Misión\n Crear experiencias únicas y personalizadas, donde cada detalle está diseñado para que nuestros clientes logren desconectarse de la rutina y se sumerjan en un oasis de calma y relajación, en completa armonía con la naturaleza.',
+                        style: TextStyle(fontSize: 16),
+                        textAlign: TextAlign.justify,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 16),
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black),
+                    ),
+                    height: 150,
+                    child: const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        'Visión\n Ser el referente en bienestar, conocido por innovar en tratamientos que no solo cuidan el cuerpo, sino que también revitalizan el espíritu, ofreciendo un refugio perfecto para la mente y el cuerpo.',
+                        style: TextStyle(fontSize: 16),
+                        textAlign: TextAlign.justify,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 16),
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black),
+                    ),
+                    height: 150,
+                    child: const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        'Calidad\n Nos comprometemos a mantener los más altos estándares en cada servicio, asegurando que cada cliente reciba atención personalizada, utilizando productos de la mejor calidad y técnicas avanzadas en cada tratamiento.',
+                        style: TextStyle(fontSize: 16),
+                        textAlign: TextAlign.justify,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black),
+                      ),
+                      height: 250,
+                      child: const Padding(
+                        padding: EdgeInsets.all(16.0),
+                        child: Text(
+                          'Contactos:\n\nTeléfono:\nEmail:\nDirección:',
+                          style: TextStyle(fontSize: 16),
+                          textAlign: TextAlign.left,
                         ),
                       ),
                     ),
                   ),
-                ],
-              ),
-            ),
-            // El ListView ocupa el espacio restante debajo del banner
-            Expanded(
-              child: ListView(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0), // Margen lateral
-                    child: Column(
-                      children: [
-                        // Carrusel de imágenes
-                        Container(
-                          decoration: BoxDecoration(
-                            color: const Color.fromARGB(255, 241, 147, 181).withOpacity(0.9), // Fondo del contenedor con opacidad
-                            borderRadius: BorderRadius.circular(15), // Bordes redondeados
-                          ),
-                          height: 450, // Ajusta la altura según sea necesario
-                          child: Row(
-                            children: [
-                              Expanded(
-                                flex: 2, // Ocupa la primer mitad del espacio
-                                child: InfiniteCarousel.builder(
-                                  controller: _carouselController,
-                                  itemCount: images.length,
-                                  itemExtent: 400,
-                                  center: true,
-                                  anchor: 0.0,
-                                  velocityFactor: 0.5,
-                                  onIndexChanged: (index) {
-                                    print('index: $index');
-                                  },
-                                  itemBuilder: (context, itemIndex, realIndex) {
-                                    return Container(
-                                      margin: const EdgeInsets.all(10),
-                                      child: Image.asset(
-                                        images[itemIndex],
-                                        fit: BoxFit.cover,
-                                      ),
-                                    );
-                                  },
-                                ),
-                              ),
-                              const Expanded(
-                                flex: 1,
-                                child: Padding(
-                                  padding: EdgeInsets.all(16),
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        'Buscamos atraer la atención de nuestros clientes a través de experiencias inspiradas en la seducción de los sentidos. Adaptamos las propuestas con el objetivo de que logre desconectarse completamente de la rutina y disfrute de un momento de bienestar, en total armonía con la naturaleza.',
-                                        style: TextStyle(color: Colors.white, fontSize: 28),
-                                        textAlign: TextAlign.justify,
-                                        maxLines: 15,
-                                        overflow: TextOverflow.ellipsis,
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black),
+                      ),
+                      height: 250,
+                      child: const Center(
+                        child: Text(
+                          'Ubicación',
+                          style: TextStyle(fontSize: 16),
+                          textAlign: TextAlign.center,
                         ),
-                        const SizedBox(height: 20), // Espacio entre el carrusel y el formulario
-
-                        // Formulario visual para dejar un testimonio y sección de testimonios pre-cargados
-                        Container(
-                          decoration: BoxDecoration(
-                            color: const Color.fromARGB(255, 241, 147, 181).withOpacity(0.9),
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                          padding: const EdgeInsets.all(16.0),
-                          child: Row(
-                            children: [
-                              // Formulario a la izquierda
-                              Expanded(
-                                flex: 1,
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    const Text(
-                                      'Cuentanos tu experiencia',
-                                      style: TextStyle(
-                                        fontSize: 24,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    const SizedBox(height: 16),
-                                    const TextField(
-                                      decoration: InputDecoration(
-                                        labelText: 'Nombre',
-                                        border: OutlineInputBorder(),
-                                        fillColor: Colors.white,
-                                        filled: true,
-                                      ),
-                                    ),
-                                    const SizedBox(height: 16),
-                                    const TextField(
-                                      maxLines: 4,
-                                      decoration: InputDecoration(
-                                        labelText: 'Testimonio',
-                                        border: OutlineInputBorder(),
-                                        fillColor: Colors.white,
-                                        filled: true,
-                                      ),
-                                    ),
-                                    const SizedBox(height: 16),
-                                    Center(
-                                      child: ElevatedButton(
-                                        onPressed: () {
-                                          // Implementación futura para enviar el testimonio
-                                        },
-                                        child: Text('Enviar'),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              const SizedBox(width: 20), // Espacio entre el formulario y los testimonios
-
-                              // Testimonios pre-cargados a la derecha
-                              Expanded(
-                                flex: 1,
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    const Text(
-                                      'Testimonios',
-                                      style: TextStyle(
-                                        fontSize: 24,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    const SizedBox(height: 16),
-                                    TestimonialWidget(
-                                      name: 'María G.',
-                                      text: 'El mejor spa que he visitado. Los tratamientos son increíbles y el personal es muy profesional.',
-                                    ),
-                                    const SizedBox(height: 16),
-                                    TestimonialWidget(
-                                      name: 'Carlos P.',
-                                      text: 'Me sentí completamente renovado después del masaje descontracturante. ¡Altamente recomendado!',
-                                    ),
-                                    const SizedBox(height: 16),
-                                    TestimonialWidget(
-                                      name: 'Lucía R.',
-                                      text: 'El ambiente es muy relajante, y los tratamientos faciales me dejaron la piel increíblemente suave.',
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-
-                        const SizedBox(height: 20), // Espacio entre el formulario y la sección de contacto
-
-                        // Sección de contacto
-                        Container(
-                          color: const Color.fromARGB(255, 205, 72, 117),
-                          height: 300, // Ajusta la altura según sea necesario
-                          width: double.infinity,
-                          child: const Center(
-                            child: Text('Contáctanos', style: TextStyle(color: Colors.white, fontSize: 60)),
-                          ),
-                        ),
-                      ],
+                      ),
                     ),
                   ),
                 ],
@@ -272,9 +196,6 @@ class _InicioState extends State<Inicio> {
           ],
         ),
       ),
-      endDrawer: ModalNavBar(), // Usa el ModalNavBar
     );
   }
 }
-
-
