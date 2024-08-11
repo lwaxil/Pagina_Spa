9fa730d3309494b880ed94ced0fbd3a95e0f848e
Maximiliano Landriel <maxiilandriel12@gmail.com>
2024-08-10 21:47:26 -0300

    Update inicio.dart

--- lib/widgets/inicio/inicio.dart
+++ lib/widgets/inicio/inicio.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:infinite_carousel/infinite_carousel.dart';
 import '../navbar.dart';
 
-
 class Inicio extends StatefulWidget {
   @override
   _InicioState createState() => _InicioState();
@@ -45,150 +44,164 @@ class _InicioState extends State<Inicio> {
         preferredSize: const Size.fromHeight(80), // Altura del NavBar
         child: NavBar(), // Usa el NavBar como appBar
       ),
-            body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background.jpg'), // Imagen de fondo
+            fit: BoxFit.cover,
+          ),
+        ),
         child: Column(
           children: [
-            Row(
-              children: [
-                Expanded(
-                  flex: 2,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black),
-                    ),
-                    height: 400,
-                    child: InfiniteCarousel.builder(
-                      controller: _carouselController,
-                      itemCount: images.length,
-                      itemExtent: 400,
-                      center: true,
-                      anchor: 0.0,
-                      velocityFactor: 0.5,
-                      onIndexChanged: (index) {
-                        print('index: $index');
-                      },
-                      itemBuilder: (context, itemIndex, realIndex) {
-                        return Container(
-                          margin: const EdgeInsets.all(10),
-                          child: Image.asset(
-                            images[itemIndex],
-                            fit: BoxFit.cover,
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 16),
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black),
-                    ),
-                    height: 400,
-                    child: const Center(
-                      child: Text(
-                        'Sumérgete en el bienestar',
-                        style: TextStyle(fontSize: 24),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 16),
-            Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black),
-                    ),
-                    height: 150,
-                    child: const Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        'Misión\n Crear experiencias únicas y personalizadas, donde cada detalle está diseñado para que nuestros clientes logren desconectarse de la rutina y se sumerjan en un oasis de calma y relajación, en completa armonía con la naturaleza.',
-                        style: TextStyle(fontSize: 16),
-                        textAlign: TextAlign.justify,
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 16),
-                Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black),
-                    ),
-                    height: 150,
-                    child: const Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        'Visión\n Ser el referente en bienestar, conocido por innovar en tratamientos que no solo cuidan el cuerpo, sino que también revitalizan el espíritu, ofreciendo un refugio perfecto para la mente y el cuerpo.',
-                        style: TextStyle(fontSize: 16),
-                        textAlign: TextAlign.justify,
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 16),
-                Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black),
-                    ),
-                    height: 150,
-                    child: const Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        'Calidad\n Nos comprometemos a mantener los más altos estándares en cada servicio, asegurando que cada cliente reciba atención personalizada, utilizando productos de la mejor calidad y técnicas avanzadas en cada tratamiento.',
-                        style: TextStyle(fontSize: 16),
-                        textAlign: TextAlign.justify,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 16),
             Expanded(
-              child: Row(
+              child: ListView(
+                padding: const EdgeInsets.all(16.0),
                 children: [
-                  Expanded(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black),
-                      ),
-                      height: 250,
-                      child: const Padding(
-                        padding: EdgeInsets.all(16.0),
-                        child: Text(
-                          'Contactos:\n\nTeléfono:\nEmail:\nDirección:',
-                          style: TextStyle(fontSize: 16),
-                          textAlign: TextAlign.left,
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 241, 147, 181).withOpacity(0.9),
+                            border: Border.all(color: Colors.black),
+                          ),
+                          height: 400,
+                          child: InfiniteCarousel.builder(
+                            controller: _carouselController,
+                            itemCount: images.length,
+                            itemExtent: 400,
+                            center: true,
+                            anchor: 0.0,
+                            velocityFactor: 0.5,
+                            itemBuilder: (context, itemIndex, realIndex) {
+                              return Container(
+                                margin: const EdgeInsets.all(10),
+                                child: Image.asset(
+                                  images[itemIndex],
+                                  fit: BoxFit.cover,
+                                ),
+                              );
+                            },
+                          ),
                         ),
                       ),
-                    ),
+                      const SizedBox(width: 16),
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 241, 147, 181).withOpacity(0.9),
+                            border: Border.all(color: Colors.black),
+                          ),
+                          height: 400,
+                          child: const Center(
+                            child: Text(
+                              'Sumérgete en el bienestar',
+                              style: TextStyle(fontSize: 24),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  const SizedBox(width: 16),
-                  Expanded(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black),
-                      ),
-                      height: 250,
-                      child: const Center(
-                        child: Text(
-                          'Ubicación',
-                          style: TextStyle(fontSize: 16),
-                          textAlign: TextAlign.center,
+                  const SizedBox(height: 16),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 241, 147, 181).withOpacity(0.9),
+                            border: Border.all(color: Colors.black),
+                          ),
+                          height: 150,
+                          child: const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text(
+                              'Misión\n Crear experiencias únicas y personalizadas, donde cada detalle está diseñado para que nuestros clientes logren desconectarse de la rutina y se sumerjan en un oasis de calma y relajación, en completa armonía con la naturaleza.',
+                              style: TextStyle(fontSize: 16),
+                              textAlign: TextAlign.justify,
+                            ),
+                          ),
                         ),
                       ),
-                    ),
+                      const SizedBox(width: 16),
+                      Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 241, 147, 181).withOpacity(0.9),
+                            border: Border.all(color: Colors.black),
+                          ),
+                          height: 150,
+                          child: const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text(
+                              'Visión\n Ser el referente en bienestar, conocido por innovar en tratamientos que no solo cuidan el cuerpo, sino que también revitalizan el espíritu, ofreciendo un refugio perfecto para la mente y el cuerpo.',
+                              style: TextStyle(fontSize: 16),
+                              textAlign: TextAlign.justify,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 16),
+                      Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 241, 147, 181).withOpacity(0.9),
+                            border: Border.all(color: Colors.black),
+                          ),
+                          height: 150,
+                          child: const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text(
+                              'Calidad\n Nos comprometemos a mantener los más altos estándares en cada servicio, asegurando que cada cliente reciba atención personalizada, utilizando productos de la mejor calidad y técnicas avanzadas en cada tratamiento.',
+                              style: TextStyle(fontSize: 16),
+                              textAlign: TextAlign.justify,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 205, 72, 117),
+                            border: Border.all(color: Colors.black),
+                          ),
+                          height: 250,
+                          child: const Padding(
+                            padding: EdgeInsets.all(16.0),
+                            child: Text(
+                              'Contactos:\n\nTeléfono:\nEmail:\nDirección:',
+                              style: TextStyle(fontSize: 16),
+                              textAlign: TextAlign.left,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 16),
+                      Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 205, 72, 117),
+                            border: Border.all(color: Colors.black),
+                          ),
+                          height: 250,
+                          child: const Center(
+                            child: Text(
+                              'Ubicación',
+                              style: TextStyle(fontSize: 16),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
